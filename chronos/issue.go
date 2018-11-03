d5f37b915d8975c24b884358ff2a45e03f63a557
flavioltonon <flavioltonon@hotmail.com>
2018-11-02 22:06:47 -0300

    add Issue struct

---
+++ chronos/issue.go
@@ -0,0 +1,10 @@
+package chronos
+
+import "time"
+
+type Issue struct {
+	ID      int       `json:"id"`
+	Number  int       `json:"number"`
+	Labels  []Label   `json:"labels"`
+	Created time.Time `json:"created_at"`
+}
