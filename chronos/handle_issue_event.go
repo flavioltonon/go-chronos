7c2511cbb6a8c476f657bbfecb1095b6c8c9cdf7
flavioltonon <flavioltonon@hotmail.com>
2018-11-04 17:22:38 -0200

    add method to update reopened issues deadlines

--- chronos/handle_issue_event.go
+++ chronos/handle_issue_event.go
@@ -23,8 +23,6 @@ func (h *IssuesEventHandler) HandleEvent(event interface{}) error {
 	log.Println(fmt.Sprintf("Event: Issue #%d has been %s", issuesEvent.GetIssue().GetNumber(), issuesEvent.GetAction()))
 
 	switch issuesEvent.GetAction() {
-	case "opened":
-		return nil
 	case "labeled":
 		log.Println("Label:", issuesEvent.GetLabel().GetName())
 
@@ -41,6 +39,18 @@ func (h *IssuesEventHandler) HandleEvent(event interface{}) error {
 		return nil
 	case "unlabeled":
 		log.Println("Label:", issuesEvent.GetLabel().GetName())
+		return nil
+	case "reopened":
+		for _, label := range issuesEvent.GetIssue().Labels {
+			if strings.Split(label.GetName(), ": ")[0] == PRIORITY_LABEL_SIGNATURE {
+				chronos.SetRequest(ChronosUpdateSingleIssueDeadlineRequest{
+					IssueNumber: issuesEvent.GetIssue().GetNumber(),
+					LabelName:   label.GetName(),
+					Created:     issuesEvent.GetIssue().GetCreatedAt(),
+				})
+				return chronos.UpdateSingleIssueDeadline()
+			}
+		}
 		return nil
 	default:
 		return nil
